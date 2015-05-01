0263c8c7b7877290a69f581536affd9492806550
cleriphil <cleri.phil@gmail.com>
2015-05-01 11:00:56 -0700

    Add and pass Definition.all, #save and .clear

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,8 +1,22 @@
 class Definition
-  attr_reader(:meaning, :part_of_speech)
+  attr_reader(:meaning, :part_of_speech, :id)
+  @@definitions = []
   define_method(:initialize) do |attributes|
     @meaning = attributes.fetch(:meaning)
     @part_of_speech = attributes.fetch(:part_of_speech)
-
+    @id = @@definitions.length().+(1)
   end
+
+  define_singleton_method(:all) do
+    @@definitions
+  end
+
+  define_method(:save) do
+    @@definitions.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@definitions = []
+  end
+
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,10 +2,33 @@ require('rspec')
 require('definition')
 
 describe(Definition) do
+  before() do
+    Definition.clear()
+  end
   describe('#meaning') do
     it('returns the meaning of the word') do
       baby = Definition.new({ :meaning => "a small human", :part_of_speech => "noun"})
       expect(baby.meaning()).to(eq("a small human"))
+    end
+  end
+  describe('.all') do
+    it('starts as an empty array') do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it('saves the definition to an array of saved definitions') do
+      baby = Definition.new({ :meaning => "a small human", :part_of_speech => "noun"})
+      baby.save()
+      expect(Definition.all()).to(eq([baby]))
+    end
+  end
+  describe('.clear') do
+    it('clears the array of saved definitions') do
+      baby = Definition.new({ :meaning => "a small human", :part_of_speech => "noun"})
+      baby.save()
+      Definition.clear()
+      expect(Definition.all()).to(eq([]))
     end
   end
 end
